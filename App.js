7fefa8fe3f92cd8b3cf7559231be0a57dc0ab135
himanshulal9 <himanshulal9@gmail.com>
2021-05-10 23:43:56 +0530

    added messages screen

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
+import MessagesScreen from "./app/screens/messagesScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <MessagesScreen />;
 }
---
+++ app/screens/MessagesScreen.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { FlatList } from "react-native";
+import ListItem from "../components/ListItem";
+import Screen from "../components/Screen";
+
+const messages = [
+  {
+    id: 1,
+    title: "title1",
+    description: "description1",
+    image: require("../assets/mosh.jpg"),
+  },
+  {
+    id: 2,
+    title: "title2",
+    description: "description2",
+    image: require("../assets/mosh.jpg"),
+  },
+];
+
+function MessagesScreen(props) {
+  return (
+    <Screen>
+      <FlatList
+        data={messages}
+        keyExtractor={(message) => message.id}
+        renderItem={(item) => (
+          <ListItem
+            image={item.image}
+            title={item.title}
+            subTitle={item.description}
+          />
+        )}
+      />
+    </Screen>
+  );
+}
+
+export default MessagesScreen;
